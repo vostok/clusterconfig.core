75a512e4ed0f8fda37067b34630fba6e851d7cce
a.tolstov <a.tolstov@skbkontur.ru>
2022-01-28 15:04:40 +0500

    feat: ClusterConfigHeaderNames and ClusterConfigQueryParameters

---
+++ Vostok.ClusterConfig.Core/Http/ClusterConfigHeaderNames.cs
@@ -0,0 +1,8 @@
+namespace Vostok.ClusterConfig.Core.Http
+{
+    internal static class ClusterConfigHeaderNames
+    {
+        public const string RecommendedProtocol = "ClusterConfig-RecommendedProtocol";
+        public const string ReplicaName = "ClusterConfig-Replica";
+    }
+}
---
+++ Vostok.ClusterConfig.Core/Http/ClusterConfigQueryParameters.cs
@@ -0,0 +1,10 @@
+namespace Vostok.ClusterConfig.Core.Http
+{
+    internal static class ClusterConfigQueryParameters
+    {
+        public const string ForceFullQueryKey = "forceFull";
+
+        public const string ForceFullReasonProtocolChanged = "protocolChanged";
+        public const string ForceFullReasonNoPrevious = "noPrevious";
+    }
+}
