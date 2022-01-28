19a28b40d2fba192ef5da97efb3b194366a61651
a.tolstov <a.tolstov@skbkontur.ru>
2022-01-28 15:24:02 +0500

    refactor: Remove ForceFullReasons

--- Vostok.ClusterConfig.Core/Http/ClusterConfigQueryParameters.cs
+++ Vostok.ClusterConfig.Core/Http/ClusterConfigQueryParameters.cs
@@ -2,12 +2,6 @@ namespace Vostok.ClusterConfig.Core.Http
 {
     internal static class ClusterConfigQueryParameters
     {
-        public const string ForceFullKey = "forceFull";
-
-        public static class ForceFullReason
-        {
-            public const string ProtocolChanged = "protocolChanged";
-            public const string NoPrevious = "noPrevious";   
-        }
+        public const string ForceFull = "forceFull";
     }
 }
