788ed751ae6377280e29fb2e7f8f5b3a62103ea8
a.tolstov <a.tolstov@skbkontur.ru>
2022-01-31 17:41:30 +0500

    feat: More handy hash methods

--- Vostok.ClusterConfig.Core/Utils/Hasher.cs
+++ Vostok.ClusterConfig.Core/Utils/Hasher.cs
@@ -5,12 +5,14 @@ namespace Vostok.ClusterConfig.Core.Utils
 {
     public static class Hasher
     {
+        public static byte[] GetSha256(this byte[] buffer) => buffer.GetSha256(0, buffer.Length);
         public static byte[] GetSha256(this byte[] buffer, int offset, int length)
         {
             using (var sha256 = SHA256.Create())
                 return sha256.ComputeHash(buffer, offset, length);
         }
 
+        public static string GetSha256Str(this byte[] buffer) => buffer.GetSha256Str(0, buffer.Length);
         public static string GetSha256Str(this byte[] buffer, int offset, int length) => Convert.ToBase64String(buffer.GetSha256(offset, length));
     }
 }
