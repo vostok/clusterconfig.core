988804ab29f9c367003cf6c44a9c36d3127f5a06
a.tolstov <a.tolstov@skbkontur.ru>
2022-01-31 17:06:07 +0500

    feat: Hasher

---
+++ Vostok.ClusterConfig.Core/Utils/Hasher.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Security.Cryptography;
+
+namespace Vostok.ClusterConfig.Core.Utils
+{
+    public static class Hasher
+    {
+        public static byte[] GetSha256(this byte[] buffer, int offset, int length)
+        {
+            using (var sha256 = SHA256.Create())
+                return sha256.ComputeHash(buffer, offset, length);
+        }
+
+        public static string GetSha256Str(this byte[] buffer, int offset, int length) => Convert.ToBase64String(buffer.GetSha256(offset, length));
+    }
+}
