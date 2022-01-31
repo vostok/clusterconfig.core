be69180f875d7c8829811baae3ce41aae4729ffa
a.tolstov <a.tolstov@skbkontur.ru>
2022-01-31 16:48:29 +0500

    feat: add ClusterConfigHeaderNames.ZoneHash

--- Vostok.ClusterConfig.Core/Http/ClusterConfigHeaderNames.cs
+++ Vostok.ClusterConfig.Core/Http/ClusterConfigHeaderNames.cs
@@ -4,5 +4,6 @@ namespace Vostok.ClusterConfig.Core.Http
     {
         public const string RecommendedProtocol = "ClusterConfig-RecommendedProtocol";
         public const string ReplicaName = "ClusterConfig-Replica";
+        public const string ZoneHash = "ClusterConfig-ZoneHash";
     }
 }
